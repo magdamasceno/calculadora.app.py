410dc46b34f38f2d982c52e31d5c4d9af36d85a0
MAG <magdamascenodireito@gmail.com>
2025-06-12 18:03:28 -0300

    Update calculadora_reclameaqui_corrigida.py

--- calculadora_reclameaqui_corrigida.py
+++ calculadora_reclameaqui_corrigida.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import math
 
-st.set_page_config(page_title="Calculadora Reclame AQUI", layout="centered")
+st.set_page_config(page_title="Calculadora RA", layout="centered")
 
 # Estilo CSS customizado
 st.markdown(
